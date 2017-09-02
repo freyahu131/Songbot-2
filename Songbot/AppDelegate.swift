63610ec40f2073711396e49ca4856a4eede394be
mac <mac@macde-MacBook-Pro.local>
2017-09-03 07:31:34 +0800

    api.ai finished

--- Songbot/AppDelegate.swift
+++ Songbot/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import ApiAI
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let configuratioin: AIConfiguration = AIDefaultConfiguration ()
+        configuratioin.clientAccessToken = "25c3d2abc89c4efbbcc7536ea549e090"
+        let apiai = ApiAI .shared()
+        apiai?.configuration = configuratioin
+       
         return true
     }
 
